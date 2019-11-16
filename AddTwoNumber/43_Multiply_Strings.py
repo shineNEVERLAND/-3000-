bd0bd6eb31cbe4479697e5134b5920df0cf52261
Wenjing Cheng <chengwenjing@bytedance.com>
2019-11-16 18:59:48 +0800

    43 Multiply Strings
    Change-Id: I3892949d05da796fe6ae9cbf2dcf1121cd2a378f

--- AddTwoNumber/43_Multiply_Strings.py
+++ AddTwoNumber/43_Multiply_Strings.py
@@ -1,3 +1,20 @@
+# coding: utf-8
+
+"""
+    8 9  <- num2
+    7 6  <- num1
+-------
+    5 4
+  4 8
+  6 3
+5 6
+-------
+6 7 6 4
+
+num1 中位置为i的数字乘以 num2 中位置为j的数字，那么得到的两位数字的位置为 i+j 和 i+j+1
+"""
+
+
 class Solution(object):
     def multiply(self, num1, num2):
         """
@@ -10,8 +27,8 @@ class Solution(object):
             carry = 0
             for j in xrange(len(num2) - 1, -1, -1):
                 tmp = int(num1[i]) * int(num2[j]) + carry
-                carry = (res[i + j + 1] + tmp) // 10
-                res[i + j + 1] = (res[i + j + 1] + tmp) % 10
+                carry = (res[i + j + 1] + tmp) // 10  # 需要进位的数
+                res[i + j + 1] = (res[i + j + 1] + tmp) % 10  # 当前位数
             res[i] += carry
         s = ''.join(map(str, res))
         return "0" if not s.lstrip("0") else s.lstrip("0")
